8e400fefbf386b903137ed0b0c87e3b3062c03d3
chaserstice <chaserstice@gmail.com>
2013-05-15 16:10:15 -0600

    Added the index.php file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "This is another test";
+        echo "how do we leave comments on the code we leave?";
+        echo '59';
+        ?>
+    </body>
+</html>
