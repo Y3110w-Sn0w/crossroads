e4a8b3eedbef0ff71d5f09bd5440c592b2863d91
chaserstice <chaserstice@gmail.com>
2013-05-18 11:53:12 -0600

    Added the main structure to our index page.

--- index.php
+++ index.php
@@ -5,6 +5,22 @@
         <title>Crossroads</title>
     </head>
     <body>
-        
+        <div class="main">
+
+            <h1>CROSSROADS</h1>
+
+            <div class="scrolling">
+
+                <a><img/>images to go here.</a>
+
+            </div>
+
+            <div class="menu">
+
+                <a><img/>Menu optoins here</a>
+            
+            </div>
+
+        </div>
     </body>
 </html>
