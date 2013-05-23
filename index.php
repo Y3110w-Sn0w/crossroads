927cb25315708fb0196adba612e03bcb8925308f
Isaac Andrade <isaac.nic@gmail.com>
2013-05-22 23:06:19 -0600

    Finished blocking things up css-wise

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <title>Food Meets Learning</title>
         <link rel="stylesheet" type="text/css" href="/stylesheets/style.css">
         <script>
@@ -16,11 +17,23 @@
             <h1 class="top-bar"><span>C</span><span>R</span><span>O</span><span>S</span><span>S</span><span>R</span><span>O</span><span>A</span><span>D</span><span>S</span></h1>
 
             <div class="scrolling">
-
-                <a><img/>images to go here.</a>
-                <a><img/>images to go here.</a>
-                <a><img/>images to go here.</a>
-                <a><img/>images to go here.</a>
+                <div>
+                    <a><img/>images to go here.</a>
+                    <a><img/>images to go here.</a>
+                    <a><img/>images to go here.</a>
+                    <a><img/>images to go here.</a>
+                    <a><img/>images to go here.</a>
+                    <a><img/>images to go here.</a>
+                    <a><img/>images to go here.</a>
+                    <a><img/>images to go here.</a>
+                    <a><img/>images to go here.</a>
+                    <a><img/>images to go here.</a>
+                    <a><img/>images to go here.</a>
+                    <a><img/>images to go here.</a>
+                    <a><img/>images to go here.</a>
+                    <a><img/>images to go here.</a>
+                    <br><a class="blank-anchor">&nbsp;</a>
+                </div>
 
             </div>
 
