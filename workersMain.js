0657460ba622fe0387ffea0c6dc50c4dd83bc924
chaserstice <chaserstice@gmail.com>
2013-06-17 16:16:49 -0600

    we finally got the worker to return data!

--- workersMain.js
+++ workersMain.js
@@ -1,6 +1,15 @@
 //this would be a worker. This runs on its own. Cannot access 'document'.
-self.addEventListener('message',function(){
-	var hr = new XMLHttpRequest();
+self.onmessage = function(e){
+    if (e.data === "start") {
+        done()
+    }
+};
+
+function done() {
+    postMessage("done");
+}
+
+	/* var hr = new XMLHttpRequest();
     hr.open("GET", "booths.json", true);
     hr.setRequestHeader("Content-Type", "application/json",true);
     hr.onreadystatechange = function(){
@@ -11,10 +20,12 @@ self.addEventListener('message',function(){
             var array_for_main = new Array();
             //next step is to take the image tag from each of the objects in the array.
             for(var i = 0; i < return_data.Booths.length; i++){
-                array_for_main = return_data.Booths[i].image;
+                array_for_main[i] = return_data.Booths[i].image;
             }
             postMessage(array_for_main);
+            self.close();
+        }else if(hr.readyState == 4){
+            postMessage("Error: " + hr.status);
+            self.close();
         }
-    }
-    hr.send(null);
-}
+    } */
